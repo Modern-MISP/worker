9521efe700aa14c285d1fede05124f5ca3c0e57c
Christopher Worsch <uxrxs@student.kit.edu>
2024-01-04 19:06:24 +0100

    contactEmail fertig bis auf send email, utilityKlasse erstellt und zum teil befüllt

--- kit/worker/email_worker/alert_email_worker.py
+++ kit/worker/email_worker/alert_email_worker.py
@@ -4,4 +4,10 @@ from kit.worker.worker import Worker
 class AlertEmailWorker(Worker):
 
     def run(self, event_id: int, old_publish: str):
+
+        #find(event_id)
+
+
+
+
         pass
--- kit/worker/email_worker/contact_email_worker.py
+++ kit/worker/email_worker/contact_email_worker.py
@@ -1,7 +1,23 @@
+from kit.worker.email_worker.utility_email import getEmailSubjekt, getAnnounceBAseurl
 from kit.worker.worker import Worker
 
 
 class ContactEmailWorker(Worker):
 
     def run(self, event_id: int, message: str, creator_only: bool):
+
+        #getEvent(event_id)
+
+        if(creator_only):
+            #getUser(Event[creatorId)
+            pass
+        else:
+            #Datenbankabfrage getAllUsersInOrg(Event[orgId)
+            pass
+
+        getEmailSubjekt()
+
+        getAnnounceBAseurl()
+
+
         pass
