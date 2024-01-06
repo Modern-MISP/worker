c9b53d385ecbe2a1150a07aaeeda4005fca2b01d
ahmad <usjuy@student.kit.edu>
2024-01-06 15:58:14 +0100

    - added MispServer - can now changed last_pull_pushed id

--- kit/misp_database/misp_database_redis.py
+++ kit/misp_database/misp_database_redis.py
@@ -1,5 +1,16 @@
 
 class MispDatabaseRedis:
+    def set_last_pull_id(self, event_id: int, id: int) -> None:
+        pass
+
+    def get_last_pull_id(self, event_id: int) -> int:
+        pass
+
+    def set_last_push_id(self, event_id: int, id: int) -> None:
+        pass
+
+    def get_last_push_id(self, event_id: int) -> int:
+        pass
 
     def set_tc_entry(self) -> bool:
         """
