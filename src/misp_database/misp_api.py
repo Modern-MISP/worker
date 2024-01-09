8d864fbef3e69eb04a45bf003bed3d969253a0ca
Lukas Christoph Sewing <uoohl@student.kit.edu>
2024-01-10 00:46:42 +0100

    reformat api

--- src/misp_database/misp_api.py
+++ src/misp_database/misp_api.py
@@ -19,12 +19,8 @@ JsonValue: TypeAlias = str | int | float | None | JsonType
 
 class MispAPI:
     # should save for every server a last-pushed-id
-
+    '''get methoden'''
     def is_server_reachable(self, server_id: int) -> bool:
-
-        pass
-
-    def get_server_settings(self, server_id: int) -> MispServer:
         pass
 
     def set_last_pulled_id(self, server_id: int) -> bool:
@@ -32,6 +28,11 @@ class MispAPI:
 
     def set_last_pushed_id(self, server_id: int) -> bool:
         pass
+
+
+    def get_server_settings(self, server_id: int) -> MispServer:
+        pass
+
 
     def get_server_version(self, server_id: int) -> MispServerVersion:
         pass
@@ -42,8 +43,6 @@ class MispAPI:
     def fetch_galaxy_cluster(self, server_id: int, cluster_id: int, user_id: int) -> MispGalaxyCluster:
         pass
 
-    def save_cluster(self, server_id, cluster: MispGalaxyCluster) -> bool:
-        pass
 
     def get_event_ids_from_server(self, ignore_filter_rules: bool) -> List[int]:
         pass
@@ -51,22 +50,11 @@ class MispAPI:
     def fetch_event(self, event_id: int) -> MispEvent:
         pass
 
-    def save_event(self, event: MispEvent) -> bool:
-        pass
 
     def fetch_sightings(self, user_id: int, server_id: int) -> List[MispSighting]:
         pass
 
     def fetch_proposals(self, user_id: int, server_id: int) -> List[MispProposal]:
-        pass
-
-    def save_proposal(self, proposal: MispProposal) -> bool:
-        pass
-
-    def save_sightings(self, sighting: MispSighting) -> bool:
-        pass
-
-    def push_cluster(self, user_id: int, cluster: MispGalaxyCluster) -> bool:
         pass
 
     def get_sharing_groups_ids(self, server_id: int) -> List[int]:
@@ -75,15 +63,12 @@ class MispAPI:
     def upload_cluster_to_server(self, user_id: int, cluster: MispGalaxyCluster) -> bool:
         pass
 
-    def upload_event_to_server(self, event: MispEvent) -> bool:
-        pass
-
     def fetch_event_attribute(self, attribute_id: int) -> MispEventAttribute:
         pass
 
     def fetch_event_attributes(self, event_id: int) -> List[MispEventAttribute]:
         pass
-
+    '''push methoden'''
     def create_attribute(self, attribute: MispEventAttribute) -> bool:
         pass
 
@@ -100,4 +85,22 @@ class MispAPI:
         pass
 
     def __modify_attribute_tag_relationship(self, relationship: AttributeTagRelationship) -> bool:
+        pass
+
+    def upload_event_to_server(self, event: MispEvent) -> bool:
+        pass
+
+    def save_proposal(self, proposal: MispProposal) -> bool:
+        pass
+
+    def save_event(self, event: MispEvent) -> bool:
+        pass
+
+    def save_cluster(self, server_id, cluster: MispGalaxyCluster) -> bool:
+        pass
+
+    def save_sightings(self, sighting: MispSighting) -> bool:
+        pass
+
+    def push_cluster(self, user_id: int, cluster: MispGalaxyCluster) -> bool:
         pass
