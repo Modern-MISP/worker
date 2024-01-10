74ea0be69ee3cee69895cc6d79d1d6710ea8f5cd
Christopher Worsch <uxrxs@student.kit.edu>
2024-01-10 16:57:21 +0100

    alertEMail redis zugriffe hinzugefügt

--- src/misp_database/mmisp_redis.py
+++ src/misp_database/mmisp_redis.py
@@ -17,7 +17,22 @@ class MMispRedis:
     def get_last_push_id_for_event(self, event_id: int) -> int:
         pass
 
+    """
+    getlist und zähle mit forschleife, wieviele einträge
+    """
     def get_amount_of_alert_entries(self, org_id: int) -> int:
+        pass
+
+    """
+    fügt neuen timestamp hinzu, und set die ttl neu auf 24 stunden
+    """
+    def set_new_org_alert_entry(self, org_id: int) -> None:
+        pass
+
+    """
+    löscht alle alten timestamps aus liste
+    """
+    def delete_old_org_alert_entries(self, org_id) -> None:
         pass
 
     def set_last_pull_id(self, last_pull_id: int) -> None:
