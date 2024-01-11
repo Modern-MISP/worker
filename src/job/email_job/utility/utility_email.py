f2930a47aebb8d8c155b655d5b1d92b85282f3b3
Christopher Worsch <uxrxs@student.kit.edu>
2024-01-12 00:11:28 +0100

    url methode gelöscht

--- src/job/email_job/utility/utility_email.py
+++ src/job/email_job/utility/utility_email.py
@@ -1,5 +1,4 @@
 from src.misp_dataclasses.misp_event import MispEvent
-from src.misp_dataclasses.misp_user import MispUser
 
 """
 Provides functionality built emails.
@@ -14,20 +13,3 @@ class UtilityEmail:
     @staticmethod
     def get_email_subject_mark_for_event(event: MispEvent) -> str:
         return "subjekt"
-
-    """
-    Returns the baseurl of Misp, if it is set. A exception wil be thrown if not baseurl is set.
-    """
-    @staticmethod
-    def get_announce_baseurl() -> str:  # TODO exeption
-
-        """$baseurl = '';
-            if (!empty(Configure::read('MISP.external_baseurl'))) {
-                $baseurl = Configure::read('MISP.external_baseurl');
-            } else if (!empty(Configure::read('MISP.baseurl'))) {
-             $baseurl = Configure::read('MISP.baseurl');
-            }
-            return $baseurl;
-     """
-
-        return "url"
