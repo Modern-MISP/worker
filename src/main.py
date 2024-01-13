182ca3578e1937469961c7410d32248c5731171a
Lukas Christoph Sewing <uoohl@student.kit.edu>
2024-01-14 00:31:12 +0100

    changed main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from api.job_router import job_router
@@ -6,3 +7,7 @@ from api.worker_router import worker_router
 app: FastAPI = FastAPI()
 app.include_router(job_router.job_router)
 app.include_router(worker_router.worker_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
