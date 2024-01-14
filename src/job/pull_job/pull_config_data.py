0db62b03e888f7de54c10c97886eefd3752833b8
ahmad <usjuy@student.kit.edu>
2024-01-14 20:09:49 +0100

    - current_user_id is now private

--- src/job/pull_job/pull_config_data.py
+++ src/job/pull_job/pull_config_data.py
@@ -6,6 +6,6 @@ class PullConfigData(ConfigData):
         super().__init__()
         self.__misp_enable_event_blocklisting: bool
         self.__misp_enable_org_blocklisting: bool
-        self.current_user_id: int
+        self.__current_user_id: int
         self.__misp_host_org_id: int
         self.__misp_enable_synchronisation_filtering_on_type: bool
