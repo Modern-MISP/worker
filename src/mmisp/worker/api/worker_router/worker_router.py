78b555ebcd700abcd63eeaf4ab68343e80d98c92
Christopher Worsch <uxrxs@student.kit.edu>
2024-01-22 00:29:38 +0100

    worker status angefangen

--- src/mmisp/worker/api/worker_router/worker_router.py
+++ src/mmisp/worker/api/worker_router/worker_router.py
@@ -1,3 +1,7 @@
+import os
+
+import dictionary
+from celery import app, Celery
 from fastapi import APIRouter
 
 from mmisp.worker.api.worker_router.input_data import WorkerEnum
@@ -5,6 +9,8 @@ from mmisp.worker.api.worker_router.response_data import (StartStopWorkerRespons
 from mmisp.worker.job.correlation_job.job_data import ChangeThresholdResponse, ChangeThresholdData
 from mmisp.worker.job.correlation_job.plugins.correlation_plugin_info import CorrelationPluginInfo
 from mmisp.worker.job.enrichment_job.plugins.enrichment_plugin import EnrichmentPluginInfo
+
+from src.mmisp.worker.api.worker_router.response_data import WorkerStatusEnum
 
 worker_router = APIRouter(prefix="/worker")
 
@@ -16,13 +22,28 @@ def enable_worker(name: WorkerEnum) -> StartStopWorkerResponse:
 
 @worker_router.post("/{name}/disable")
 def disable_worker(name: WorkerEnum) -> StartStopWorkerResponse:
-    """test"""
+    os.popen('pkill -9 -f ' + name)
     return StartStopWorkerResponse()
 
 
 @worker_router.get("/{name}/status")
 def get_worker_status(name: WorkerEnum) -> WorkerStatusResponse:
-    return {"status": "success"}
+    """TODO den celery_app bums in Konstruktor?"""
+
+    celery_app = Celery('worker', broker='redis:')
+    report: dictionary = celery_app.control.inspect().active
+
+    response: StartStopWorkerResponse = StartStopWorkerResponse()
+    response.jobs_queued = celery_app.control.inspect.reserved()[name]
+
+    if report.get(name) is None:
+        response.status = WorkerStatusEnum.DEACTIVATED
+    elif report.get(name).isempty():
+        response.status = WorkerStatusEnum.IDLE
+    else:
+        response.status = WorkerStatusEnum.WORKING
+
+    return response
 
 
 @worker_router.get("/enrichment/plugins")
