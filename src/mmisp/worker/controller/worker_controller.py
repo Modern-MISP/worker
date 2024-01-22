7f161efec2d2c94a51ad44fbaa40e179553e7de1
Christopher Worsch <uxrxs@student.kit.edu>
2024-01-22 15:11:50 +0100

    für pull

--- src/mmisp/worker/controller/worker_controller.py
+++ src/mmisp/worker/controller/worker_controller.py
@@ -1,5 +1,7 @@
 from typing import Self
 
+import dictionary
+
 from src.mmisp.worker.api.worker_router.input_data import WorkerEnum
 
 from src.mmisp.worker.exceptions.singleton_exception import SingletonException
@@ -29,4 +31,19 @@ class WorkerController:
         pass
 
     def disable_worker(self, name: WorkerEnum) -> None:
-        pass
+        """TODO den celery_app bums in Konstruktor?"""
+
+        celery_app = Celery('worker', broker='redis:')
+        report: dictionary = celery_app.control.inspect().active
+
+        response: WorkerStatusResponse = WorkerStatusResponse()
+        response.jobs_queued = len(celery_app.control.inspect.reserved()[name])
+
+        if report.get(name) is None:
+            response.status = WorkerStatusEnum.DEACTIVATED
+        elif report.get(name).isempty():
+            response.status = WorkerStatusEnum.IDLE
+        else:
+            response.status = WorkerStatusEnum.WORKING
+
+        return response
