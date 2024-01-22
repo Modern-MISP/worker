b2069e93d7022854657109c02fc4ff8464652ddd
Christopher Worsch <uxrxs@student.kit.edu>
2024-01-22 16:14:47 +0100

    get_worker_status implemented

--- src/mmisp/worker/api/worker_router/worker_router.py
+++ src/mmisp/worker/api/worker_router/worker_router.py
@@ -1,16 +1,15 @@
 import os
 
-import dictionary
-from celery import app, Celery
 from fastapi import APIRouter
 
 from src.mmisp.worker.api.worker_router.input_data import WorkerEnum
-from src.mmisp.worker.api.worker_router.response_data import (StartStopWorkerResponse, WorkerStatusResponse)
+from src.mmisp.worker.api.worker_router.response_data import (StartStopWorkerResponse, WorkerStatusResponse,
+                                                              WorkerStatusEnum)
+from src.mmisp.worker.controller.worker_controller import WorkerController
 from src.mmisp.worker.job.correlation_job.job_data import ChangeThresholdResponse, ChangeThresholdData
 from src.mmisp.worker.job.correlation_job.plugins.correlation_plugin_info import CorrelationPluginInfo
 from src.mmisp.worker.job.enrichment_job.plugins.enrichment_plugin import EnrichmentPluginInfo
 
-from src.mmisp.worker.api.worker_router.response_data import WorkerStatusEnum
 
 worker_router = APIRouter(prefix="/worker")
 
@@ -30,7 +29,21 @@ def disable_worker(name: WorkerEnum) -> StartStopWorkerResponse:
 
 @worker_router.get("/{name}/status")
 def get_worker_status(name: WorkerEnum) -> WorkerStatusResponse:
-    return WorkerStatusResponse()
+
+    worker_controller: WorkerController = WorkerController.get_instance()
+
+    response: WorkerStatusResponse = WorkerStatusResponse()
+
+    if worker_controller.is_worker_online(name):
+        if worker_controller.is_worker_active(name):
+            response.status = WorkerStatusEnum.WORKING
+        else:
+            response.status = WorkerStatusEnum.IDLE
+    else:
+        response.status = WorkerStatusEnum.DEACTIVATED
+
+    response.jobs_queued = worker_controller.get_job_count(name)
+    return response
 
 
 @worker_router.get("/enrichment/plugins")
--- src/mmisp/worker/controller/worker_controller.py
+++ src/mmisp/worker/controller/worker_controller.py
@@ -3,6 +3,7 @@ from typing import Self
 import dictionary
 
 from src.mmisp.worker.api.worker_router.input_data import WorkerEnum
+from src.mmisp.worker.api.worker_router.response_data import WorkerStatusResponse, WorkerStatusEnum
 
 from src.mmisp.worker.exceptions.singleton_exception import SingletonException
 
@@ -22,28 +23,39 @@ class WorkerController:
             raise SingletonException("Attempted to create a second instance of the 'WorkerController' class.")
 
     def is_worker_online(self, name: WorkerEnum) -> bool:
-        pass
+
+        """TODO celery"""
+        celery_app = Celery('worker', broker='redis:')
+
+
+        report: dictionary = celery_app.control.inspect().active
+        if report.get(name) is None:
+            return False
+
+        return True
+
+
 
     def is_worker_active(self, name: WorkerEnum) -> bool:
-        pass
+        """TODO celery"""
+        celery_app = Celery('worker', broker='redis:')
+
+
+        report: dictionary = celery_app.control.inspect().active
+
+        if report.get(name).isempty():
+            return False
+
+        return True
+
+    def get_job_count(self, name: WorkerEnum) -> int:
+        """TODO celery"""
+        celery_app = Celery('worker', broker='redis:')
+
+        return len(celery_app.control.inspect.reserved()[name])
 
     def enable_worker(self, name: WorkerEnum) -> None:
         pass
 
     def disable_worker(self, name: WorkerEnum) -> None:
-        """TODO den celery_app bums in Konstruktor?"""
-
-        celery_app = Celery('worker', broker='redis:')
-        report: dictionary = celery_app.control.inspect().active
-g
-        response: WorkerStatusResponse = WorkerStatusResponse()
-        response.jobs_queued = len(celery_app.control.inspect.reserved()[name])
-
-        if report.get(name) is None:
-            response.status = WorkerStatusEnum.DEACTIVATED
-        elif report.get(name).isempty():
-            response.status = WorkerStatusEnum.IDLE
-        else:
-            response.status = WorkerStatusEnum.WORKING
-
-        return response
+        pass
