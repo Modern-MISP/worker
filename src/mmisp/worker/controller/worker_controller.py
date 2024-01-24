05a607bb2e972fba63c06ef98e0d39ec94f29991
Christopher Worsch <uxrxs@student.kit.edu>
2024-01-24 15:50:59 +0100

    pydoc worker_controller

--- src/mmisp/worker/controller/worker_controller.py
+++ src/mmisp/worker/controller/worker_controller.py
@@ -1,9 +1,12 @@
 import os
-from typing import Self
 
 from mmisp.worker.api.worker_router.response_data import StartStopWorkerResponse
 from mmisp.worker.controller.celery.celery import celery_app
 from mmisp.worker.api.worker_router.input_data import WorkerEnum
+
+"""
+Encapsulates the logic of the API for the worker router 
+"""
 
 
 class WorkerController:
@@ -14,6 +17,13 @@ class WorkerController:
 
     @staticmethod
     def is_worker_online(name: WorkerEnum) -> bool:
+        """
+        Checks if the specified worker is online
+        :param name: Contains the name of the worker
+        :type name: WorkerEnum
+        :return: True if the worker online, else False
+        :rtype: bool
+        """
         report: dict = celery_app.control.inspect().active
         if report.get(name.value) is None:
             return False
@@ -22,6 +32,14 @@ class WorkerController:
 
     @staticmethod
     def is_worker_active(name: WorkerEnum) -> bool:
+        """
+        Checks if the specified worker is active
+        :param name: Contains the name of the worker
+        :type name: WorkerEnum
+        :return: True if the worker active, else False
+        :rtype: bool
+
+        """
         report: dict = celery_app.control.inspect().active
 
         if report.get(name.value).isempty():
@@ -31,27 +49,43 @@ class WorkerController:
 
     @staticmethod
     def get_job_count(name: WorkerEnum) -> int:
+        """
+        Returns the number of jobs in the specified worker queue
+        :param name: Contains the name of the worker
+        :type name: WorkerEnum
+        :return: The amount of jobs in the worker queue
+        :rtype: int
+        """
         return len(celery_app.control.inspect.reserved()[name.value])
 
     @staticmethod
     def enable_worker(name: WorkerEnum) -> StartStopWorkerResponse:
-
+        """
+        Enables the specified worker,if it is not already enabled
+        :param name: Contains the name of the worker
+        :type name: WorkerEnum
+        :return: A response containing information about the success of enabling the worker
+        :rtype: StartStopWorkerResponse
+        """
         if WorkerController.is_worker_online(name):
             return StartStopWorkerResponse(saved=True, success=False, name=WorkerController.__ALREADY_ENABLED,
                                            message=WorkerController.__ALREADY_ENABLED,
                                            url="/worker/" + name.value + "/enable")
         else:
-            # TODO
-            pid_path: str = ""
-            os.popen(f'celery -A main.celery worker -Q {name.value} ~--loglevel = info - n {name.value} - '
-                     f'-pidfile {pid_path} ')
+            os.popen(f'celery -A main.celery worker -Q {name.value} --loglevel = info - n {name.value} --concurrency 1')
             return StartStopWorkerResponse(saved=True, success=True, name=WorkerController.__NOW_ENABLED,
                                            message=WorkerController.__NOW_ENABLED,
                                            url="/worker/" + name.value + "/enable")
 
     @staticmethod
     def disable_worker(name: WorkerEnum) -> StartStopWorkerResponse:
-
+        """
+        Disables the specified worker,if it is not already disabled
+        :param name: Contains the name of the worker
+        :type name: WorkerEnum
+        :return: A response containing information about the success of disabling the worker
+        :rtype: StartStopWorkerResponse
+        """
         if WorkerController.is_worker_online(name):
             os.popen('pkill -9 -f ' + name.value)
 
