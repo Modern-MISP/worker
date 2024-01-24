671fa59ebe4259b841167d34f750ac4984807b11
Amadeus Haessler <mail@amadeushaessler.de>
2024-01-24 21:59:20 +0100

    Added Docstring for celery package.

--- src/mmisp/worker/controller/celery/config.py
+++ src/mmisp/worker/controller/celery/config.py
@@ -1,10 +1,11 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class CeleryConfig:
+    """
+    Encapsulates configuration for Celery.
+    """
+
     broker_url: str = os.environ.get("CELERY_BROKER_URL", "redis://172.17.0.2:12482/0")
     result_backend: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:9443/0")
     task_routes: dict = {}
