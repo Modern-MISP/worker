807b9864cf5ad77535457f81e4d203532a0a503e
Amadeus Haessler <mail@amadeushaessler.de>
2024-02-04 23:40:44 +0100

    Implemented entrypoing.

--- src/mmisp/worker/main.py
+++ src/mmisp/worker/main.py
@@ -1,18 +1,64 @@
+import signal
+import sys
+
+import uvicorn
 from fastapi import FastAPI
 
 from mmisp.worker import test
 from mmisp.worker.api.job_router import job_router
 from mmisp.worker.api.worker_router import worker_router
+from mmisp.worker.api.worker_router.input_data import WorkerEnum
+from mmisp.worker.controller.worker_controller import WorkerController
+from mmisp.worker.system_config_data import SystemConfigData
+
+"""
+The main module of the MMISP Worker application.
+"""
 
 app: FastAPI = FastAPI()
+"""The FastAPI instance."""
+
 app.include_router(job_router.job_router)
 app.include_router(worker_router.worker_router)
 
 
 def main():
+    """
+    The entry point of the MMISP Worker application.
+    Starts the enabled workers and sets up the API.
+    """
+
+    # TODO: Remove before release
     test.run()
-    # uvicorn.run("main:app", port=5000, log_level="info")
+
+    config: SystemConfigData = SystemConfigData()
+    config.read_from_env()
+
+    for worker in WorkerEnum:
+        if config.is_autostart_for_worker_enabled(worker):
+            WorkerController.enable_worker(worker)
+
+    uvicorn.run(f"{__name__}:app", port=config.api_port, log_level="info")
+    # stop_workers()
+
+
+def interrupt_handler(signum, frame) -> None:
+    # stop_workers()
+    sys.exit(130)
+
+
+def terminate_handler(signum, frame) -> None:
+    # stop_workers()
+    sys.exit(143)
+
+
+def stop_workers() -> None:
+    print("Stopping workers...")
+    for worker in WorkerEnum:
+        WorkerController.disable_worker(worker)
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, interrupt_handler)
+    signal.signal(signal.SIGTERM, terminate_handler)
     main()
