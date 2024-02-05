ed1d928bfe758596cac104e494d1a9b573a339ff
ahmad <usjuy@student.kit.edu>
2024-02-05 14:48:56 +0100

    - fixed requirements.txt (missed mysql-connector-python~=8.3.0) - added get_session(server_id) in MispApi - created a Test-MispAI class

--- tests/unittests/api/test_basic_api_endpoints.py
+++ tests/unittests/api/test_basic_api_endpoints.py
@@ -1,18 +1,19 @@
 from unittest import TestCase
 
-from mmisp.worker.misp_database.misp_api import MispAPI
 from mmisp.worker.misp_dataclasses.misp_server import MispServer
+from mmisp.worker.misp_dataclasses.misp_server_version import MispServerVersion
+from tests.unittests.api.test_misp_api import TestMispAPI
 
 
 class TestBasicApiEndpoints(TestCase):
     def test_get_server(self):
-        misp_api: MispAPI = MispAPI()
+        misp_api: TestMispAPI = TestMispAPI()
         server: MispServer = misp_api.get_server(1)
         self.assertEqual(server.name, "MISP 02")
 
     def test_get_server_version(self):
-        misp_api: MispAPI = MispAPI()
+        misp_api: TestMispAPI = TestMispAPI()
         server: MispServer = misp_api.get_server(1)
 
-        version: str = misp_api.get_server_version(server)
-        self.assertEqual(version, "2.4.178")
+        version: MispServerVersion = misp_api.get_server_version(server)
+        self.assertEqual(version.version, "2.4.178")
