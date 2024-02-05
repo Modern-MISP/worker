f6f60818ff6d618a681bbbb441ebd99874a4e52d
Lukas Christoph Sewing <uoohl@student.kit.edu>
2024-02-05 18:59:22 +0100

    updated domain and filemane testcases

--- tests/unittests/jobs/processfreetext/test_domain_filename_validator.py
+++ tests/unittests/jobs/processfreetext/test_domain_filename_validator.py
@@ -8,47 +8,47 @@ from mmisp.worker.misp_dataclasses.attribute_type import AttributeType
 
 class BTCTestcase(unittest.TestCase):
 
-    def test_validate_domain_filename(self):
-        result_dictionary = {
-            'hostname': AttributeType(types=['hostname', 'domain', 'url', 'filename'], default_type='hostname',
-                                      value=''),
-            'domain': AttributeType(types=['domain', 'filename'], default_type='domain',
-                                    value=''),
-            'link': AttributeType(types=['link'], default_type='link', value=''),
-            'url': AttributeType(types=['url'], default_type='url', value=''),
-            'filename': AttributeType(types=['filename'], default_type='filename', value=''),
-            'regkey': AttributeType(types=['regkey'], default_type='regkey', value='')
-        }
+    def test_validate_hostname(self):
+        testcases = ['test.example.com']
+        for testcase in testcases:
+            result = DomainFilenameTypeValidator().validate(testcase)
+            self.assertEqual(result, AttributeType(types=['hostname', 'domain', 'url', 'filename'], default_type='hostname', value=testcase))
 
-        test_dictionary = [
-            {'from': 'test.example.com', 'to': 'hostname'},  # valid
-            {'from': 'test.com', 'to': 'domain'},  # valid
-            {'from': 'https://www.example.com/test', 'to': 'url'},  # valid
-            {'from': 'https://www.example.com/test?param1=value1&param2=value2', 'to': 'url'},  # valid
-            {'from': 'https://virustotal.com', 'to': 'link'},  # valid
-            {'from': 'example.txt', 'to': 'filename'},  # valid
-            {'from': 'document.pdf', 'to': 'filename'},  # valid
-            {'from': 'image.jpeg', 'to': 'filename'},  # valid
-            {'from': r'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion', 'to': 'regkey'},  # valid
-            {'from': 'example.123', 'to': False}, # invalid
-            {'from': 'subdomain!.example.com', 'to': 'filename'},  # valid
-            {'from': 'my_domain.com!', 'to': False},  # invalid
-            {'from': 'ftp://www.example.com', 'to': 'url'},  # valid
-            {'from': 'htps://sub.example.com', 'to': 'filename'},  # valid
-            {'from': 'invalid-link', 'to': False}, # invalid
-            {'from': 'my_file%$#.txt', 'to': 'filename'},  # valid
-            {'from': 'HKLM\System\InvalidKey', 'to': 'regkey'},  # valid
-            {'from': 'HKCU\Software\KeyWithoutSpaces', 'to': 'regkey'},  # valid
+    def test_validate_domain(self):
+        testcases = ['test.com', 'example.com']
+        for testcase in testcases:
+            result = DomainFilenameTypeValidator().validate(testcase)
+            self.assertEqual(result, AttributeType(types=['domain', 'filename'], default_type='domain', value=testcase))
 
+    def test_validate_url(self):
+        testcases = ['https://www.example.com/test', 'https://www.example.com/test?param1=value1&param2=value2','ftp://www.example.com']
+        for testcase in testcases:
+            result = DomainFilenameTypeValidator().validate(testcase)
+            self.assertEqual(result, AttributeType(types=['url'], default_type='url', value=testcase))
 
-        ]
-        for testcase in test_dictionary:
-            result = DomainFilenameTypeValidator().validate(testcase["from"])
-            if testcase["to"]:
-                result_dictionary[testcase["to"]].value = testcase["from"]
-                self.assertEqual(result, result_dictionary[testcase["to"]])
-            else:
-               self.assertEqual(result, None)
+    def test_validate_filename(self):
+        testcases = ['example.txt', 'document.pdf', 'image.jpeg', 'subdomain!.example.com']
+        for testcase in testcases:
+            result = DomainFilenameTypeValidator().validate(testcase)
+            self.assertEqual(result, AttributeType(types=['filename'], default_type='filename', value=testcase))
+
+    def test_validate_regkey(self):
+        testcases = ['HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion','HKLM\System\InvalidKey', 'HKCU\Software\KeyWithoutSpaces']
+        for testcase in testcases:
+            result = DomainFilenameTypeValidator().validate(testcase)
+            self.assertEqual(result, AttributeType(types=['regkey'], default_type='regkey', value=testcase))
+
+    def test_validate_link(self):
+        testcases = ['https://virustotal.com']
+        for testcase in testcases:
+            result = DomainFilenameTypeValidator().validate(testcase)
+            self.assertEqual(result, AttributeType(types=['link'], default_type='link', value=testcase))
+
+    def test_validate_invalid(self):
+        testcases = ['example.123', 'my_domain.com!', 'invalid-link']
+        for testcase in testcases:
+            result = DomainFilenameTypeValidator().validate(testcase)
+            self.assertIsNone(result)
 
     def test_validate_is_link(self):
         self.assertEqual(DomainFilenameTypeValidator()._is_link('https://virustotal.com'), True)
