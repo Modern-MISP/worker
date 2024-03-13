af18d74226122ac769e1891b6a0c197c62bbb1be
Lukas Sewing <lukas.sewing@student.kit.edu>
2024-03-13 13:52:37 +0100

    initial tests

--- tests/system_tests/jobs/processfreetext/systemtest.py
+++ tests/system_tests/jobs/processfreetext/systemtest.py
@@ -13,16 +13,16 @@ data = {
 def simple_testcase():
     start_worker_response = requests.post(url+"worker/processFreeText/enable", headers=headers).json()
     if start_worker_response["success"] == True or start_worker_response["message"] == 'Processfreetext-Worker already enabled':
-    create_response = requests.post(url + "job/processFreeText", headers=headers, json=data).json()
-    print(create_response)
-    job_id = create_response["job_id"]
-    status = False
-    while not status:
-        status_response = requests.get(url + f"job/{job_id}/status", headers=headers).json()
-        if status_response["status"] == "success":
-            status = True
-            print("here")
-    print(requests.get(url + f"job/{job_id}/result", headers=headers).json())
+        create_response = requests.post(url + "job/processFreeText", headers=headers, json=data).json()
+        job_id = create_response["job_id"]
+        status = False
+        while not status:
+            status_response = requests.get(url + f"job/{job_id}/status", headers=headers).json()
+            if status_response["status"] == "success":
+                status = True
+        result = requests.get(url + f"job/{job_id}/result", headers=headers).json()
+    else:
+        raise IOError
 
 
 
