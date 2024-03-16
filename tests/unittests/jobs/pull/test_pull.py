cdc1a3441fb2c547f427b4c853cd2ed0408ee7f3
ahmad <usjuy@student.kit.edu>
2024-03-16 18:17:23 +0100

    - fixed pull and push - added get_event_from_uuid - fixed pull and push for uuid

--- tests/unittests/jobs/pull/test_pull.py
+++ tests/unittests/jobs/pull/test_pull.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from mmisp.worker.api.job_router.input_data import UserData
 from mmisp.worker.jobs.sync.pull.job_data import PullData, PullResult
-from tests.mocks.sync.pull_job import pull_job
+from tests.mocks.sync.pull.test_pull_job import test_pull_job
 
 
 class TestPull(TestCase):
@@ -11,14 +11,14 @@ class TestPull(TestCase):
         user_data: UserData = UserData(user_id=1)
         pull_data: PullData = PullData(server_id=1, technique="full")
 
-        result: PullResult = pull_job(user_data, pull_data)
+        result: PullResult = test_pull_job(user_data, pull_data)
         self.assertEqual(1, 1)
 
     def test_pull_incremental(self):
         user_data: UserData = UserData(user_id=1)
         pull_data: PullData = PullData(server_id=1, technique="incremental")
 
-        result: PullResult = pull_job(user_data, pull_data)
+        result: PullResult = test_pull_job(user_data, pull_data)
         self.assertEqual(1, 1)
 
 
@@ -26,5 +26,5 @@ class TestPull(TestCase):
         user_data: UserData = UserData(user_id=1)
         pull_data: PullData = PullData(server_id=1, technique="pull_relevant_clusters")
 
-        result: PullResult = pull_job(user_data, pull_data)
+        result: PullResult = test_pull_job(user_data, pull_data)
         self.assertEqual(1, 1)
--- tests/unittests/jobs/push/test_push.py
+++ tests/unittests/jobs/push/test_push.py
@@ -2,9 +2,9 @@ from unittest import TestCase
 
 from mmisp.worker.api.job_router.input_data import UserData
 from mmisp.worker.jobs.sync.pull.job_data import PullData, PullResult
-from mmisp.worker.jobs.sync.pull.pull_job import pull_job
+from mmisp.worker.jobs.sync.pull.pull_job import test_pull_job
 from mmisp.worker.jobs.sync.push.job_data import PushResult, PushData
-from mmisp.worker.jobs.sync.push.push_job import push_job
+from tests.mocks.sync.push.test_push_job import test_push_job
 
 
 class TestPush(TestCase):
@@ -13,12 +13,12 @@ class TestPush(TestCase):
         user_data: UserData = UserData(user_id=1)
         push_data: PushData = PushData(server_id=1, technique="full")
 
-        result: PushResult = push_job(user_data, push_data)
+        result: PushResult = test_push_job(user_data, push_data)
         self.assertEqual(1, 1)
 
     def test_push_incremental(self):
         user_data: UserData = UserData(user_id=1)
         push_data: PushData = PushData(server_id=1, technique="incremental")
 
-        result: PushResult = push_job(user_data, push_data)
+        result: PushResult = test_push_job(user_data, push_data)
         self.assertEqual(1, 1)
