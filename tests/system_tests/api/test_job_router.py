2ffa00edf534d313010a08ba1c2ae9a2b0658db6
Christopher Worsch <uxrxs@student.kit.edu>
2024-03-17 22:16:36 +0100

    test optimiert, damit sichergestellt wird, dass queues leer sind wenn test gestartet wird

--- tests/system_tests/api/test_job_router.py
+++ tests/system_tests/api/test_job_router.py
@@ -20,6 +20,8 @@ class TestJobRouter(TestCase):
     }
 
     def test_get_job_status_success(self):
+        assert requests.get(url + "/worker/enrichment/status", headers=headers).json()["jobs_queued"] == 0, \
+            "Worker queue is not empty"
 
         data: json = {
             "user": {
@@ -41,6 +43,8 @@ class TestJobRouter(TestCase):
         self.assertTrue(check_status(request.json()["job_id"]))
 
     def test_get_job_status_failed(self):
+        assert requests.get(url + "/worker/sendEmail/status", headers=headers).json()["jobs_queued"] == 0, \
+            "Worker queue is not empty"
 
         requests.post(url + "/worker/sendEmail/enable", headers=headers)
 
@@ -93,7 +97,13 @@ class TestJobRouter(TestCase):
 
         self.assertEqual(expected_output, response)
 
+        # to ensure that the job is finished and the worker is free again for other tests
+        self.assertTrue(check_status(job_id))
+
     def test_get_job_status_queued(self):
+        assert requests.get(url + "/worker/enrichment/status", headers=headers).json()["jobs_queued"] == 0, \
+            "Worker queue is not empty"
+
         requests.post(url + "/worker/enrichment/disable", headers=headers)
 
         request = requests.post(url + "/job/enrichAttribute", headers=headers, json=self._dummy_body)
@@ -111,7 +121,13 @@ class TestJobRouter(TestCase):
 
         self.assertEqual(expected_output, response)
 
+        # to ensure that the job is finished and the worker is free again for other tests
+        self.assertTrue(check_status(job_id))
+
     def test_get_job_status_revoked_worker_enabled(self):
+        assert requests.get(url + "/worker/enrichment/status", headers=headers).json()["jobs_queued"] == 0, \
+            "Worker queue is not empty"
+
         requests.post(url + "/worker/enrichment/enable", headers=headers)
 
         requests.post(url + "/job/enrichAttribute", headers=headers, json=self._dummy_body)
@@ -137,6 +153,10 @@ class TestJobRouter(TestCase):
         self.assertEqual(expected_output, response)
 
     def test_get_job_status_revoked_worker_disabled(self):
+
+        assert requests.get(url + "/worker/enrichment/status", headers=headers).json()["jobs_queued"] == 0, \
+            "Worker queue is not empty"
+
         # one worker has to be enabled to ensure that the job will be canceled
         requests.post(url + "/worker/sendEmail/enable", headers=headers)
 
@@ -147,7 +167,7 @@ class TestJobRouter(TestCase):
         if request.status_code != 200:
             self.fail("Job could not be created")
 
-        sleep(4)
+        sleep(1)
 
         job_id: int = request.json()["job_id"]
 
@@ -169,8 +189,12 @@ class TestJobRouter(TestCase):
         self.assertEqual(expected_output, response)
 
     def test_remove_job(self):
+        assert requests.get(url + "/worker/enrichment/status", headers=headers).json()["jobs_queued"] == 0, \
+            "Worker queue is not empty"
+
         requests.post(url + "/worker/enrichment/disable", headers=headers)
 
+        requests.post(url + "/job/enrichAttribute", headers=headers, json=self._dummy_body)
         request = requests.post(url + "/job/enrichAttribute", headers=headers, json=self._dummy_body)
 
         if request.status_code != 200:
@@ -178,7 +202,9 @@ class TestJobRouter(TestCase):
 
         job_id: int = request.json()["job_id"]
 
-        sleep(4)
+        requests.post(url + "/worker/enrichment/enable", headers=headers)
+
+        sleep(5)
 
         cancel_resp = requests.delete(url + f"/job/{job_id}/cancel", headers=headers)
 
