65018ef6640ba465aa737f9576679f5ad3499894
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-10-14 23:46:25 +0200

    jobs move __init__.py to fastapi_endpoints.py

--- src/mmisp/worker/jobs/all_jobs.py
+++ src/mmisp/worker/jobs/all_jobs.py
@@ -3,10 +3,13 @@ import itertools
 
 jobs_pkg = "mmisp.worker.jobs"
 
-all_jobs_pkgs = (
-    i.name for i in importlib.resources.files(jobs_pkg).iterdir() if i.is_dir() and any(i.glob("__init__.py"))
+all_jobs_pkgs = list(
+    i.name
+    for i in importlib.resources.files(jobs_pkg).iterdir()
+    if i.is_dir() and all([any(i.glob("__init__.py")), any(i.glob("fastapi_endpoints.py"))])
 )
-jobs_pkg_names = map(".".join, zip(itertools.repeat(jobs_pkg), all_jobs_pkgs))
+print(all_jobs_pkgs)
+jobs_pkg_names = map(".".join, zip(itertools.repeat(jobs_pkg), all_jobs_pkgs, itertools.repeat("fastapi_endpoints")))
 
 for m in jobs_pkg_names:
     importlib.import_module(m)
