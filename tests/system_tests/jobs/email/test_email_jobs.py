18a68e9ddc2e43e860d7a4ca4254c9d8c1e7061a
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-10-15 14:47:50 +0200

    refactor test_email_jobs

--- tests/system_tests/jobs/email/test_email_jobs.py
+++ tests/system_tests/jobs/email/test_email_jobs.py
@@ -1,104 +1,102 @@
 import uuid
-from typing import Self
-from unittest import TestCase
 
-import pytest
-from pydantic import json
-
+from mmisp.worker.controller import worker_controller
 from tests.system_tests.request_settings import headers, old_misp_headers, old_misp_url
 from tests.system_tests.utility import check_status
 
+# user_id of the user who should receive the email, make sure this user exists with an email address you can check
+_user_id: int = 52
 
-@pytest.mark.usefixtures("client_class")
-class TestEmailJobs(TestCase):
-    # user_id of the user who should receive the email, make sure this user exists with an email address you can check
-    _user_id: int = 52
 
-    def _create_event(self: Self) -> int:
-        event_json: json = {
-            "object_id": 0,
-            "object_relation": None,
-            "category": "Internal reference",
-            "type": "hex",
-            "to_ids": True,
-            "uuid": str(uuid.uuid4()),
-            "timestamp": 1706092974,
-            "distribution": 0,
-            "sharing_group_id": 0,
-            "comment": None,
-            "deleted": False,
-            "disable_correlation": False,
-            "first_seen": None,
-            "last_seen": None,
-            "value": "12345678900",
-            "event_uuid": None,
-            "data": None,
-            "info": "edited info",
-            "tags": [],
-        }
-        event_request = self.client.post(old_misp_url + "/events/add", headers=old_misp_headers, json=event_json)
+def _create_event(client) -> int:
+    event_json = {
+        "object_id": 0,
+        "object_relation": None,
+        "category": "Internal reference",
+        "type": "hex",
+        "to_ids": True,
+        "uuid": str(uuid.uuid4()),
+        "timestamp": 1706092974,
+        "distribution": 0,
+        "sharing_group_id": 0,
+        "comment": None,
+        "deleted": False,
+        "disable_correlation": False,
+        "first_seen": None,
+        "last_seen": None,
+        "value": "12345678900",
+        "event_uuid": None,
+        "data": None,
+        "info": "edited info",
+        "tags": [],
+    }
+    event_request = client.post(old_misp_url + "/events/add", headers=old_misp_headers, json=event_json)
 
-        if event_request.status_code != 200:
-            self.fail("Event could not be created")
+    if event_request.status_code != 200:
+        assert False, "Event could not be created"
 
-        return event_request.json()["Event"]["id"]
+    return event_request.json()["Event"]["id"]
 
-    def test_alert_email_job(self: Self):
-        self.client.post("/worker/sendEmail/disable", headers=headers)
 
-        body: json = {
-            "user": {"user_id": 1},
-            "data": {
-                "event_id": self._create_event(),
-                "old_publish": "1706736785",
-                "receiver_ids": [self._user_id],
-            },
-        }
+def test_alert_email_job(client):
+    worker_controller.pause_all_workers()
 
-        request = self.client.post("/job/alertEmail", json=body, headers=headers)
-        if request.status_code != 200:
-            self.fail("Job could not be created")
+    body = {
+        "user": {"user_id": 1},
+        "data": {
+            "event_id": _create_event(client),
+            "old_publish": "1706736785",
+            "receiver_ids": [_user_id],
+        },
+    }
 
-        self.client.post("/worker/sendEmail/enable", headers=headers)
+    request = client.post("/job/alertEmail", json=body, headers=headers)
+    if request.status_code != 200:
+        assert False, "Job could not be created"
 
-        self.assertTrue(check_status(request.json()["job_id"], self.client))
+    worker_controller.reset_worker_queues()
 
-    def test_contact_email(self: Self):
-        self.client.post("/worker/sendEmail/disable", headers=headers)
+    assert check_status(request.json()["job_id"], client)
 
-        body: json = {
-            "user": {"user_id": self._user_id},
-            "data": {
-                "event_id": self._create_event(),
-                "message": "test message",
-                "receiver_ids": [self._user_id],
-            },
-        }
 
-        request = self.client.post("/job/contactEmail", json=body, headers=headers)
-        if request.status_code != 200:
-            self.fail("Job could not be created")
+def test_contact_email(client):
+    worker_controller.pause_all_workers()
 
-        self.client.post("/worker/sendEmail/enable", headers=headers)
+    body = {
+        "user": {"user_id": _user_id},
+        "data": {
+            "event_id": _create_event(client),
+            "message": "test message",
+            "receiver_ids": [_user_id],
+        },
+    }
 
-        self.assertTrue(check_status(request.json()["job_id"], self.client))
+    request = client.post("/job/contactEmail", json=body, headers=headers)
+    if request.status_code != 200:
+        assert False, "Job could not be created"
 
-    """
-    Make sure the post_id exists
-    """
+    worker_controller.reset_worker_queues()
 
-    def test_posts_email(self: Self):
-        self.client.post("/worker/sendEmail/disable", headers=headers)
+    assert check_status(request.json()["job_id"], client)
 
-        body: json = {
-            "user": {"user_id": 1},
-            "data": {"post_id": 5, "title": "test", "message": "test message", "receiver_ids": [self._user_id]},
-        }
 
-        request = self.client.post("/job/postsEmail", json=body, headers=headers)
-        if request.status_code != 200:
-            self.fail("Job could not be created")
+"""
+Make sure the post_id exists
+"""
 
-        self.client.post("/worker/sendEmail/enable", headers=headers)
 
-        self.assertTrue(check_status(request.json()["job_id"], self.client))
+def test_posts_email(client):
+    worker_controller.pause_all_workers()
+
+    body = {
+        "user": {"user_id": 1},
+        "data": {"post_id": 5, "title": "test", "message": "test message", "receiver_ids": [_user_id]},
+    }
+
+    request = client.post("/job/postsEmail", json=body, headers=headers)
+    if request.status_code != 200:
+        assert False, "Job could not be created"
+
+    worker_controller.reset_worker_queues()
+
+    assert check_status(request.json()["job_id"], client)
