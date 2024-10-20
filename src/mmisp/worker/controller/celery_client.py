c9aa6f1ade3b771f2fa4818c6ebfe1d1775010f7
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-10-21 00:04:41 +0200

    add all_model import to celery client

--- src/mmisp/worker/controller/celery_client.py
+++ src/mmisp/worker/controller/celery_client.py
@@ -12,6 +12,7 @@ from celery.signals import before_task_publish, celeryd_after_setup
 from celery.worker.consumer import Consumer
 from celery.worker.control import control_command
 
+import mmisp.db.all_models  # noqa
 from mmisp.worker.api.requests_schemas import WorkerEnum
 from mmisp.worker.config import ENV_PREFIX
 from mmisp.worker.misp_database.mmisp_redis_config import mmisp_redis_config_data
