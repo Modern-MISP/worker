ac9fc8725cc74c0fd378d45944c68b4402ba76fc
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-10-24 20:14:01 +0200

    fix enrichment plugin startup worker

--- src/mmisp/worker/worker_main.py
+++ src/mmisp/worker/worker_main.py
@@ -1,3 +1,5 @@
+import os
+
 from mmisp.worker.config import ENV_PREFIX
 from mmisp.worker.controller.celery_client import celery_app
 from mmisp.worker.jobs.enrichment.plugins.enrichment_plugin_factory import enrichment_plugin_factory
@@ -6,8 +8,10 @@ from mmisp.worker.plugins.loader import PluginLoader
 
 def main() -> None:
     # load enrichment plugins
-    enrichment_plugin_env_dir = f"{ENV_PREFIX}_ENRICHMENT_PLUGIN_DIRECTORY"
-    PluginLoader.load_plugins_from_directory(enrichment_plugin_env_dir, enrichment_plugin_factory)
+    enrichment_plugin_env = f"{ENV_PREFIX}_ENRICHMENT_PLUGIN_DIRECTORY"
+    enrichment_plugin_dir = os.environ.get(enrichment_plugin_env, None)
+    if enrichment_plugin_dir is not None:
+        PluginLoader.load_plugins_from_directory(enrichment_plugin_dir, enrichment_plugin_factory)
 
     worker = celery_app.Worker()
     worker.start()
