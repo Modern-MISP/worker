a160c1574838d1aded1e5964c2412fd6263d1d75
Christopher Worsch <uxrxs@student.kit.edu>
2024-10-27 18:54:18 +0100

    added dummy_body methode and fixture

--- tests/system_tests/api/test_job_router.py
+++ tests/system_tests/api/test_job_router.py
@@ -1,15 +1,32 @@
 from time import sleep
 
+import pytest_asyncio
+from plugins.enrichment_plugins.blocking_plugin import BlockingPlugin
 from pydantic import json
 from starlette.testclient import TestClient
 
+from mmisp.tests.generators.model_generators.attribute_generator import generate_attribute
 from mmisp.worker.controller import worker_controller
 from tests.system_tests.utility import check_status
 
-_dummy_body: json = {
-    "user": {"user_id": 3},
-    "data": {"attribute_id": 272910, "enrichment_plugins": ["Blocking Plugin"]},
-}
+
+@pytest_asyncio.fixture()
+async def attribute_matching_blocking_plugin(db, event):
+    attribute = generate_attribute(event_id=event.id)
+    attribute.value = BlockingPlugin.PLUGIN_INFO.MISP_ATTRIBUTES.INPUT[0]
+
+    event.attribute_count += 1
+
+    db.add(attribute)
+    await db.commit()
+    await db.refresh(attribute)
+
+    yield attribute
+    await db.refresh(event)
+
+    await db.delete(attribute)
+    event.attribute_count -= 1
+    await db.commit()
 
 
 def test_get_job_status_success(client: TestClient, authorization_headers):
@@ -42,16 +59,19 @@ def test_get_job_status_failed(client: TestClient, authorization_headers):
     assert expected_output == response
 
 
-def test_get_job_status_in_progress(client: TestClient, authorization_headers):
+def test_get_job_status_in_progress(client: TestClient, authorization_headers, user,
+                                    attribute_matching_blocking_plugin):
     worker_controller.pause_all_workers()
 
-    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=_dummy_body)
+    dummy_body = _get_dummy_body(user.id, attribute_matching_blocking_plugin.id)
+
+    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=dummy_body)
 
     assert (request.status_code == 200), "Job could not be created"
 
     job_id: int = request.json()["job_id"]
 
-    response: json = client.get(f"/job/{job_id}/status", headers=authorization_headers()).json()
+    response: json = client.get(f"/job/{job_id}/status", headers=authorization_headers).json()
 
     expected_output = {"message": "Job is currently being executed", "status": "inProgress"}
     worker_controller.reset_worker_queues()
@@ -61,10 +81,12 @@ def test_get_job_status_in_progress(client: TestClient, authorization_headers):
     assert expected_output == response
 
 
-def test_get_job_status_queued(client: TestClient, authorization_headers):
+def test_get_job_status_queued(client: TestClient, authorization_headers, user, attribute_matching_blocking_plugin):
     worker_controller.pause_all_workers()
 
-    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=_dummy_body)
+    dummy_body = _get_dummy_body(user.id, attribute_matching_blocking_plugin.id)
+
+    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=dummy_body)
 
     assert (request.status_code == 200), "Job could not be created"
 
@@ -81,10 +103,11 @@ def test_get_job_status_queued(client: TestClient, authorization_headers):
     assert expected_output == response
 
 
-def test_get_job_status_revoked_worker_enabled(client: TestClient, authorization_headers):
-    client.post("/job/enrichAttribute", headers=authorization_headers, json=_dummy_body)
+def test_get_job_status_revoked_worker_enabled(client: TestClient, authorization_headers, user,
+                                               attribute_matching_blocking_plugin):
+    dummy_body = _get_dummy_body(user.id, attribute_matching_blocking_plugin.id)
 
-    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=_dummy_body)
+    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=dummy_body)
 
     assert (request.status_code == 200), "Job could not be created"
 
@@ -103,10 +126,13 @@ def test_get_job_status_revoked_worker_enabled(client: TestClient, authorization
     assert expected_output == response
 
 
-def test_get_job_status_revoked_worker_disabled(client: TestClient, authorization_headers):
+def test_get_job_status_revoked_worker_disabled(client: TestClient, authorization_headers, user,
+                                                attribute_matching_blocking_plugin):
     worker_controller.pause_all_workers()
 
-    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=_dummy_body)
+    dummy_body = _get_dummy_body(user.id, attribute_matching_blocking_plugin.id)
+
+    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=dummy_body)
 
     assert (request.status_code == 200), "Job could not be created"
 
@@ -127,11 +153,13 @@ def test_get_job_status_revoked_worker_disabled(client: TestClient, authorizatio
     assert expected_output == response
 
 
-def test_remove_job(client: TestClient, authorization_headers):
+def test_remove_job(client: TestClient, authorization_headers, user, attribute_matching_blocking_plugin):
     worker_controller.pause_all_workers()
 
-    client.post("/job/enrichAttribute", headers=authorization_headers, json=_dummy_body)
-    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=_dummy_body)
+    dummy_body = _get_dummy_body(user.id, attribute_matching_blocking_plugin.id)
+
+    client.post("/job/enrichAttribute", headers=authorization_headers, json=dummy_body)
+    request = client.post("/job/enrichAttribute", headers=authorization_headers, json=dummy_body)
 
     assert (request.status_code == 200), "Job could not be created"
 
@@ -146,3 +174,10 @@ def test_remove_job(client: TestClient, authorization_headers):
     expected_output = {"success": True}
 
     assert expected_output == cancel_resp.json()
+
+
+def _get_dummy_body(user_id, attribute_matching_blocking_plugin_id):
+    return {
+        "user": {"user_id": user_id},
+        "data": {"attribute_id": attribute_matching_blocking_plugin_id, "enrichment_plugins": ["Blocking Plugin"]}
+    }
