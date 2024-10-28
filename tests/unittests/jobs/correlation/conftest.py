20b1e6984fd4a1fbf953be64176e4ee9271494e8
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-10-28 15:49:44 +0100

    move fixture into conftest

---
+++ tests/unittests/jobs/correlation/conftest.py
@@ -0,0 +1,27 @@
+import pytest_asyncio
+
+from mmisp.db.models.attribute import Attribute
+from mmisp.db.models.event import Event
+from mmisp.tests.generators.model_generators.attribute_generator import generate_text_attribute
+
+from .fixtures import CORRELATION_VALUE
+
+
+@pytest_asyncio.fixture
+async def correlation_test_event(db, event) -> Event:
+    attributes: list[Attribute] = []
+
+    for i in range(2):
+        attribute: Attribute = generate_text_attribute(event.id, CORRELATION_VALUE)
+        db.add(attribute)
+        await db.commit()
+        await db.refresh(attribute)
+        attributes.append(attribute)
+
+    await db.refresh(event)
+    yield event
+
+    for attribute in attributes:
+        await db.delete(attribute)
+    await db.commit()
+    await db.refresh(event)
