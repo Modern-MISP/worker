1b73f9d37d87c3cf211350c68909822c8de9c0e5
Christopher Worsch <uxrxs@student.kit.edu>
2024-10-29 15:02:44 +0100

    (correlation) trying to find out if i get another respons code

--- tests/unittests/jobs/correlation/test_correlate_value_job.py
+++ tests/unittests/jobs/correlation/test_correlate_value_job.py
@@ -54,7 +54,8 @@ async def test_over_correlating_value(db, event):
 
 @pytest.mark.asyncio
 async def test_found_correlations(init_api_config, correlation_test_event, correlation_test_event_2):
-    assert correlation_test_event.uuid != correlation_test_event_2.uuid
+    assert correlation_test_event.id != correlation_test_event_2.id
+
     test_data: CorrelateValueData = CorrelateValueData(value=CORRELATION_VALUE)
     result: CorrelateValueResponse = await _correlate_value_job(user, test_data)
 
