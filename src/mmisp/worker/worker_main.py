c3d074fc9b4036900e63ccb99c877ef97eec5fb9
Christopher Worsch <uxrxs@student.kit.edu>
2024-10-29 13:19:12 +0100

    added correlation_pluginloading in worker_main

--- src/mmisp/worker/worker_main.py
+++ src/mmisp/worker/worker_main.py
@@ -2,6 +2,7 @@ import os
 
 from mmisp.worker.config import ENV_PREFIX
 from mmisp.worker.controller.celery_client import celery_app
+from mmisp.worker.jobs.correlation.plugins.correlation_plugin_factory import correlation_plugin_factory
 from mmisp.worker.jobs.enrichment.plugins.enrichment_plugin_factory import enrichment_plugin_factory
 from mmisp.worker.plugins.loader import PluginLoader
 
@@ -9,9 +10,15 @@ from mmisp.worker.plugins.loader import PluginLoader
 def main() -> None:
     # load enrichment plugins
     enrichment_plugin_env = f"{ENV_PREFIX}_ENRICHMENT_PLUGIN_DIRECTORY"
-    enrichment_plugin_dir = os.environ.get(enrichment_plugin_env, None)
-    if enrichment_plugin_dir is not None:
-        PluginLoader.load_plugins_from_directory(enrichment_plugin_dir, enrichment_plugin_factory)
+    correlation_plugin_dir = os.environ.get(enrichment_plugin_env, None)
+    if correlation_plugin_dir is not None:
+        PluginLoader.load_plugins_from_directory(correlation_plugin_dir, enrichment_plugin_factory)
+
+    # load correlation plugins
+    correlation_plugin_env = f"{ENV_PREFIX}_CORRELATION_PLUGIN_DIRECTORY"
+    correlation_plugin_dir = os.environ.get(correlation_plugin_env, None)
+    if correlation_plugin_dir is not None:
+        PluginLoader.load_plugins_from_directory(correlation_plugin_dir, correlation_plugin_factory)
 
     worker = celery_app.Worker()
     worker.start()
