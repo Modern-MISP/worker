cfdcaac01ae5eacdf9078387acb1e068be4e0af8
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-11-01 13:50:34 +0100

    fix posts email job

--- tests/with_smtp/email/test_posts_email_job.py
+++ tests/with_smtp/email/test_posts_email_job.py
@@ -13,7 +13,8 @@ def test_posts_email_job(init_api_config, post, instance_owner_org_admin_user, s
     data: PostsEmailData = PostsEmailData(
         post_id=post.id, receiver_ids=[instance_owner_org_admin_user.id], message="test message", title="test title"
     )
-    posts_email_job(UserData(user_id=site_admin_user.id), data)
+    async_result = posts_email_job.delay(UserData(user_id=site_admin_user.id), data)
+    async_result.get()
 
     response = requests.get(f"http://{config.mmisp_smtp_host}:9000/api/messages")
 
