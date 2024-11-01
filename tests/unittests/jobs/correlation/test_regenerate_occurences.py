275aaaa9994020c3786d3c94c74ac6c72dfc63b4
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-11-01 19:43:33 +0100

    bump sqlalchemy dependency version

--- tests/unittests/jobs/correlation/test_regenerate_occurences.py
+++ tests/unittests/jobs/correlation/test_regenerate_occurences.py
@@ -2,13 +2,13 @@ import pytest
 
 from mmisp.worker.api.requests_schemas import UserData
 from mmisp.worker.jobs.correlation.job_data import DatabaseChangedResponse
-from mmisp.worker.jobs.correlation.regenerate_occurrences_job import _regenerate_occurrences_job
+from mmisp.worker.jobs.correlation.regenerate_occurrences_job import regenerate_occurrences_job
 
 
 @pytest.mark.asyncio
 async def test_regenerate_occurrences_job(user, correlating_value):
     # Test
     user: UserData = UserData(user_id=user.id)
-    result: DatabaseChangedResponse = await _regenerate_occurrences_job(user)
+    result: DatabaseChangedResponse = regenerate_occurrences_job.delay(user).get()
     assert result.success
     assert result.database_changed
