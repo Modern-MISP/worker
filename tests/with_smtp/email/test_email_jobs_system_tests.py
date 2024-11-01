46d7fbb86ea0acfba68a888ad4e3dcef341e3e7f
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-11-01 13:34:52 +0100

    fix alert_email_job

--- tests/with_smtp/email/test_email_jobs_system_tests.py
+++ tests/with_smtp/email/test_email_jobs_system_tests.py
@@ -4,13 +4,14 @@ from icecream import ic
 from starlette.testclient import TestClient
 
 from mmisp.worker.api.requests_schemas import UserData
-from mmisp.worker.jobs.email import alert_email_job
+from mmisp.worker.jobs.email.alert_email_job import alert_email_job
 from mmisp.worker.jobs.email.job_data import AlertEmailData, ContactEmailData, PostsEmailData
 from tests.system_tests.utility import check_status
 
 
-def test_alert_email_job(client: TestClient, authorization_headers, instance_owner_org_admin_user, event,
-                         site_admin_user):
+def test_alert_email_job(
+    client: TestClient, authorization_headers, instance_owner_org_admin_user, event, site_admin_user
+):
     """
     body = {
         "user": UserData(user_id=site_admin_user.id).dict(),
@@ -23,8 +24,9 @@ def test_alert_email_job(client: TestClient, authorization_headers, instance_own
     """
 
     user = UserData(user_id=site_admin_user.id)
-    data = AlertEmailData(event_id=event.id, old_publish="1706736785",
-                          receiver_ids=[instance_owner_org_admin_user.id]).dict()
+    data = AlertEmailData(
+        event_id=event.id, old_publish="1706736785", receiver_ids=[instance_owner_org_admin_user.id]
+    ).dict()
 
     try:
         async_result = alert_email_job.delay(user, data)
@@ -44,12 +46,14 @@ def test_alert_email_job(client: TestClient, authorization_headers, instance_own
     """
 
 
-def test_contact_email(client: TestClient, authorization_headers, instance_owner_org_admin_user, event,
-                       site_admin_user):
+def test_contact_email(
+    client: TestClient, authorization_headers, instance_owner_org_admin_user, event, site_admin_user
+):
     body = {
         "user": UserData(user_id=site_admin_user.id).dict(),
-        "data": ContactEmailData(event_id=event.id, message="test message",
-                                 receiver_ids=[instance_owner_org_admin_user.id]).dict(),
+        "data": ContactEmailData(
+            event_id=event.id, message="test message", receiver_ids=[instance_owner_org_admin_user.id]
+        ).dict(),
     }
 
     request = client.post("/job/contactEmail", json=body, headers=authorization_headers)
@@ -62,8 +66,9 @@ def test_contact_email(client: TestClient, authorization_headers, instance_owner
 def test_posts_email(client: TestClient, authorization_headers, instance_owner_org_admin_user, post, site_admin_user):
     body = {
         "user": UserData(user_id=site_admin_user.id).dict(),
-        "data": PostsEmailData(post_id=post.id, title="test", message="test message",
-                               receiver_ids=[instance_owner_org_admin_user.id]).dict(),
+        "data": PostsEmailData(
+            post_id=post.id, title="test", message="test message", receiver_ids=[instance_owner_org_admin_user.id]
+        ).dict(),
     }
 
     request = client.post("/job/postsEmail", json=body, headers=authorization_headers)
