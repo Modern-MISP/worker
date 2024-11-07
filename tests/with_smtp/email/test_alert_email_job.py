2e89d0db323b82bc4874bbf751c341e4da2c21df
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-11-07 10:38:49 +0100

    fix test_alert_email_job

--- tests/with_smtp/email/test_alert_email_job.py
+++ tests/with_smtp/email/test_alert_email_job.py
@@ -6,7 +6,8 @@ from mmisp.worker.jobs.email.job_data import AlertEmailData
 from mmisp.worker.jobs.email.utility.email_config_data import EmailConfigData
 
 
-def test_alert_email_job(init_api_config, event, instance_owner_org_admin_user, site_admin_user):
+def test_alert_email_job(init_api_config, event_with_sharing_group, instance_owner_org_admin_user, site_admin_user):
+    event = event_with_sharing_group
     config: EmailConfigData = EmailConfigData()
     response = requests.get(f"http://{config.mmisp_smtp_host}:9000/api/messages")
 
