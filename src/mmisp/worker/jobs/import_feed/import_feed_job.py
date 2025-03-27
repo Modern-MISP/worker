fe4e1d5743b622ef271ff4c4e8108237ff935785
Kizhyk <mikita.kizhyk@student.kit.edu>
2025-03-27 17:51:55 +0100

    Fix feeds

--- src/mmisp/worker/jobs/import_feed/import_feed_job.py
+++ src/mmisp/worker/jobs/import_feed/import_feed_job.py
@@ -2,9 +2,11 @@ import asyncio
 import json
 import re
 import string
+from datetime import datetime
 
 import requests
 from celery.utils.log import get_task_logger
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 
@@ -128,10 +130,11 @@ async def _import_feed_job(user: UserData, data: ImportFeedData) -> ImportFeedRe
             for link in links:
                 try:
                     parsed_site = parse_site(link)
-                    await processmisp_job(user, parsed_site)
+                    await processmisp_job(user, parsed_site, db)
                 except IOError as e:
                     logger.error(f"Failed to parse site {link}: {e}")
                     return ImportFeedResponse(success=False, message="Wrong link format")
+            return ImportFeedResponse(success=True, message="Job executed")
 
         elif feed_to_import.source_format == "csv":
             try:
@@ -159,10 +162,9 @@ async def _import_feed_job(user: UserData, data: ImportFeedData) -> ImportFeedRe
                     )
                     new_event.attributes.extend(attributes_list)
                     db.add(new_event)
-                    await db.commit()
+                    await db.flush()
                     logger.info("new event added")
                     feed_to_import.event_id = new_event.id
-                    await db.commit()
                 logger.info("Import Feed Job completed")
                 return ImportFeedResponse(success=True, message="Job executed")
             except IOError as e:
@@ -170,168 +172,186 @@ async def _import_feed_job(user: UserData, data: ImportFeedData) -> ImportFeedRe
                 return ImportFeedResponse(success=False, message="Wrong link format")
 
 
-async def processmisp_job(user: UserData, string_to_process: str) -> Event:
-    async with sessionmanager.session() as db:
-        """Processes the given string and returns event found in it.
+async def processmisp_job(user: UserData, string_to_process: str, db: AsyncSession) -> Event:
+    """Processes the given string and returns event found in it.
 
-        This routine analyzes the provided string, identifies potential event
-        according to the MISP format and returns it.
+    This routine analyzes the provided string, identifies potential event
+    according to the MISP format and returns it.
 
-        Args:
-            user (UserData): The user who requested the processing job.
-            string_to_process (str): The string to be processed and analyzed for event.
+    Args:
+        user (UserData): The user who requested the processing job.
+        string_to_process (str): The string to be processed and analyzed for event.
+        db (AsyncSession): The asynchronous database session object.
 
-        Returns:
-            Event: Event found in the string
-        Raises:
-            ValueError: raised if the data format is not MISP
-        """
+    Returns:
+        Event: Event found in the string
+    Raises:
+        ValueError: raised if the data format is not MISP
+    """
 
-        try:
-            event_data = json.loads(string_to_process)
-            if "Event" not in event_data:
-                logger.error("Invalid MISP format: missing 'Event' key")
-                raise ValueError("Invalid MISP format: missing 'Event' key")
+    try:
+        event_data = json.loads(string_to_process)
+        if "Event" not in event_data:
+            logger.error("Invalid MISP format: missing 'Event' key")
+            raise ValueError("Invalid MISP format: missing 'Event' key")
 
-            event_attributes = event_data["Event"].get("Attribute", [])
-            event_tags = event_data["Event"].get("Tag", [])
-            event_objects = event_data["Event"].get("Object", [])
-            event_attribute_count = int(event_data["Event"].get("attribute_count", len(event_attributes)))
-            new_event = Event(
-                info=event_data["Event"]["info"],
-                uuid=event_data["Event"]["uuid"],
-                org_id=0,
-                orgc_id=0,
-                analysis=event_data["Event"].get("analysis", 0),
-                distribution=event_data["Event"].get("distribution", 0),
-                threat_level_id=event_data["Event"].get("threat_level_id", 0),
+        event_attributes = event_data["Event"].get("Attribute", [])
+        event_tags = event_data["Event"].get("Tag", [])
+        event_objects = event_data["Event"].get("Object", [])
+        event_attribute_count = int(event_data["Event"].get("attribute_count", len(event_attributes)))
+        new_event = Event(
+            info=event_data["Event"]["info"],
+            uuid=event_data["Event"]["uuid"],
+            org_id=0,
+            orgc_id=0,
+            analysis=event_data["Event"].get("analysis", 0),
+            distribution=event_data["Event"].get("distribution", 0),
+            threat_level_id=event_data["Event"].get("threat_level_id", 0),
+            user_id=0,
+            date=event_data["Event"]["date"],
+            published=event_data["Event"]["published"],
+            attribute_count=event_attribute_count,
+            timestamp=event_data["Event"]["timestamp"],
+            sharing_group_id=event_data["Event"].get("sharing_group_id", 0),
+            proposal_email_lock=event_data["Event"].get("proposal_email_lock", False),
+            locked=event_data["Event"].get("locked", False),
+            publish_timestamp=event_data["Event"].get("publish_timestamp", 0),
+            sighting_timestamp=event_data["Event"].get("sighting_timestamp", 0),
+            disable_correlation=event_data["Event"].get("disable_correlation", False),
+            extends_uuid=event_data["Event"].get("extends_uuid", ""),
+            protected=event_data["Event"].get("protected", False),
+        )
+        db.add(new_event)
+        await db.flush()
+        await db.refresh(new_event)
+        for tag in event_tags:
+            misp_tag = Tag(
+                name=tag.get("name", ""),
+                colour=tag.get("colour", "#000000"),
+                exportable=tag.get("exportable", False),
                 user_id=0,
-                date=event_data["Event"]["date"],
-                published=event_data["Event"]["published"],
-                attribute_count=event_attribute_count,
-                timestamp=event_data["Event"]["timestamp"],
-                sharing_group_id=event_data["Event"].get("sharing_group_id", 0),
-                proposal_email_lock=event_data["Event"].get("proposal_email_lock", False),
-                locked=event_data["Event"].get("locked", False),
-                publish_timestamp=event_data["Event"]["publish_timestamp"],
-                sighting_timestamp=event_data["Event"].get("sighting_timestamp", 0),
-                disable_correlation=event_data["Event"].get("disable_correlation", False),
-                extends_uuid=event_data["Event"].get("extends_uuid", ""),
-                protected=event_data["Event"].get("protected", False),
+                hide_tag=tag.get("hide_tag", False),
+                numerical_value=tag.get("numerical_value", 0),
+                is_galaxy=tag.get("is_galaxy", False),
+                is_custom_galaxy=tag.get("is_custom_galaxy", False),
+                local_only=tag.get("local_only", False),
             )
-            db.add(new_event)
-            await db.commit()
-            await db.refresh(new_event)
-            for tag in event_tags:
-                misp_tag = Tag(
-                    name=tag.get("name", ""),
-                    colour=tag.get("colour", "#000000"),
-                    exportable=tag.get("exportable", False),
-                    user_id=0,
-                    hide_tag=tag.get("hide_tag", False),
-                    numerical_value=tag.get("numerical_value", 0),
-                    is_galaxy=tag.get("is_galaxy", False),
-                    is_custom_galaxy=tag.get("is_custom_galaxy", False),
-                    local_only=tag.get("local_only", False),
-                )
-                if (await db.execute(select(Tag).where(Tag.name == misp_tag.name))).scalars().first() is None:
-                    db.add(misp_tag)
-                    await db.flush()
-                    event_tag = EventTag(event_id=new_event.id, tag_id=misp_tag.id)
-                    db.add(event_tag)
-            for attr in event_attributes:
-                attr_type = attr["type"]
-                attr_comment = attr.get("comment", "")
-                attr_uuid = attr["uuid"]
-                attr_event_id = new_event.id
-                attr_object_relation = attr.get("object_relation", "")
-                attr_value1 = attr.get("value", "")
-                attr_to_ids = attr["to_ids"]
-                attr_timestamp = attr["timestamp"]
-                attr_distribution = attr.get("distribution", 0)
-                attr_sharing_group_id = attr.get("sharing_group_id", 0)
-                attr_deleted = attr.get("deleted", False)
-                attr_disable_correlation = attr.get("disable_correlation", False)
-                attr_first_seen = attr.get("first_seen", 0)
-                attr_last_seen = attr.get("last_seen", 0)
-                attr_category = attr["category"]
-                attribute = Attribute(
-                    uuid=attr_uuid,
-                    type=attr_type,
-                    value1=attr_value1,
-                    value2="",
-                    comment=attr_comment,
-                    event_id=attr_event_id,
-                    object_relation=attr_object_relation,
-                    to_ids=attr_to_ids,
-                    timestamp=attr_timestamp,
-                    distribution=attr_distribution,
-                    sharing_group_id=attr_sharing_group_id,
-                    deleted=attr_deleted,
-                    disable_correlation=attr_disable_correlation,
-                    first_seen=attr_first_seen,
-                    last_seen=attr_last_seen,
-                    category=attr_category,
-                )
-                if (
-                    await db.execute(select(Attribute).where(Attribute.uuid == attribute.uuid))
-                ).scalars().first() is None:
-                    db.add(attribute)
-            for obj in event_objects:
-                misp_object = Object(
-                    uuid=obj.get("uuid", uuid),
-                    name=obj.get("name", "unknown"),
-                    meta_category=obj.get("meta-category", "N/A"),
-                    description=obj.get("description", ""),
-                    template_uuid=obj.get("template_uuid", ""),
-                    template_version=obj.get("template_version", 0),
-                    event_id=new_event.id,
-                    timestamp=obj.get("timestamp", 0),
-                    distribution=obj.get("distribution", 0),
-                    sharing_group_id=obj.get("sharing_group_id", 0),
-                    comment=obj.get("comment", ""),
-                    deleted=obj.get("deleted", False),
-                    first_seen=obj.get("first_seen", None),
-                    last_seen=obj.get("last_seen", None),
-                )
-                if (
-                    await db.execute(select(Object).where(Object.uuid == misp_object.uuid))
-                ).scalars().first() is not None:
-                    continue
-                db.add(misp_object)
-                for attr in obj.get("Attribute", []):
-                    object_attribute = Attribute(
-                        uuid=attr.get("uuid", uuid),
-                        object_id=misp_object.id,
-                        type=attr.get("type", "unknown"),
-                        value1=attr.get("value", ""),
-                        comment=attr.get("comment", ""),
-                        event_id=new_event.id,
-                        object_relation=attr.get("object_relation", ""),
-                        to_ids=attr.get("to_ids", True),
-                        timestamp=attr.get("timestamp", 0),
-                        distribution=attr.get("distribution", 0),
-                        sharing_group_id=attr.get("sharing_group_id", 0),
-                        deleted=attr.get("deleted", False),
-                        disable_correlation=attr.get("disable_correlation", False),
-                        first_seen=attr.get("first_seen", 0),
-                        last_seen=attr.get("last_seen", 0),
-                        category=attr.get("category", "default"),
-                    )
-                    if (
-                        await db.execute(select(Attribute).where(Attribute.uuid == object_attribute.uuid))
-                    ).scalars().first() is None:
-                        db.add(object_attribute)
-            logger.info(f"Processed Event: {new_event}")
-            await db.commit()
-            return new_event
+            if (await db.execute(select(Tag).where(Tag.name == misp_tag.name))).scalars().first() is None:
+                db.add(misp_tag)
+                await db.flush()
+                event_tag = EventTag(event_id=new_event.id, tag_id=misp_tag.id)
+                db.add(event_tag)
+        for attr in event_attributes:
+            attr_type = attr["type"]
+            attr_comment = attr.get("comment", "")
+            attr_uuid = attr["uuid"]
+            attr_event_id = new_event.id
+            attr_object_relation = attr.get("object_relation", "")
+            attr_value1 = attr.get("value", "")
+            attr_to_ids = attr["to_ids"]
+            attr_timestamp = attr["timestamp"]
+            attr_distribution = attr.get("distribution", 0)
+            attr_sharing_group_id = attr.get("sharing_group_id", 0)
+            attr_deleted = attr.get("deleted", False)
+            attr_disable_correlation = attr.get("disable_correlation", False)
+            attr_first_seen = attr.get("first_seen")
+            if attr_first_seen is not None:
+                attr_first_seen = datetime.fromisoformat(attr_first_seen).timestamp()
 
-        except json.JSONDecodeError as e:
-            logger.error(f"JSON parsing error: {e}")
-            raise ValueError(f"Invalid JSON format: {e}")
-        except Exception as e:
-            logger.error(f"Unexpected error in processmisp_job: {e}")
-            raise
+            attr_last_seen = attr.get("last_seen")
+            if attr_last_seen is not None:
+                attr_last_seen = datetime.fromisoformat(attr_last_seen).timestamp()
+            attr_category = attr["category"]
+            attribute = Attribute(
+                uuid=attr_uuid,
+                type=attr_type,
+                value1=attr_value1,
+                value2="",
+                comment=attr_comment,
+                event_id=attr_event_id,
+                object_relation=attr_object_relation,
+                to_ids=attr_to_ids,
+                timestamp=attr_timestamp,
+                distribution=attr_distribution,
+                sharing_group_id=attr_sharing_group_id,
+                deleted=attr_deleted,
+                disable_correlation=attr_disable_correlation,
+                first_seen=attr_first_seen,
+                last_seen=attr_last_seen,
+                category=attr_category,
+            )
+            if (await db.execute(select(Attribute).where(Attribute.uuid == attribute.uuid))).scalars().first() is None:
+                db.add(attribute)
+        for obj in event_objects:
+            first_seen = obj.get("first_seen")
+            if first_seen is not None:
+                first_seen = datetime.fromisoformat(first_seen).timestamp()
+
+            last_seen = obj.get("last_seen")
+            if last_seen is not None:
+                last_seen = datetime.fromisoformat(last_seen).timestamp()
+
+            misp_object = Object(
+                uuid=obj.get("uuid", uuid),
+                name=obj.get("name", "unknown"),
+                meta_category=obj.get("meta-category", "N/A"),
+                description=obj.get("description", ""),
+                template_uuid=obj.get("template_uuid", ""),
+                template_version=obj.get("template_version", 0),
+                event_id=new_event.id,
+                timestamp=obj.get("timestamp", 0),
+                distribution=obj.get("distribution", 0),
+                sharing_group_id=obj.get("sharing_group_id", 0),
+                comment=obj.get("comment", ""),
+                deleted=obj.get("deleted", False),
+                first_seen=first_seen,
+                last_seen=last_seen,
+            )
+            if (await db.execute(select(Object).where(Object.uuid == misp_object.uuid))).scalars().first() is not None:
+                continue
+            db.add(misp_object)
+
+            for attr in obj.get("Attribute", []):
+                first_seen = attr.get("first_seen")
+                if first_seen is not None:
+                    first_seen = datetime.fromisoformat(first_seen).timestamp()
+
+                last_seen = attr.get("last_seen")
+                if last_seen is not None:
+                    last_seen = datetime.fromisoformat(last_seen).timestamp()
+
+                object_attribute = Attribute(
+                    uuid=attr.get("uuid", uuid),
+                    object_id=misp_object.id,
+                    type=attr.get("type", "unknown"),
+                    value1=attr.get("value", ""),
+                    comment=attr.get("comment", ""),
+                    event_id=new_event.id,
+                    object_relation=attr.get("object_relation", ""),
+                    to_ids=attr.get("to_ids", True),
+                    timestamp=attr.get("timestamp", 0),
+                    distribution=attr.get("distribution", 0),
+                    sharing_group_id=attr.get("sharing_group_id", 0),
+                    deleted=attr.get("deleted", False),
+                    disable_correlation=attr.get("disable_correlation", False),
+                    first_seen=first_seen,
+                    last_seen=last_seen,
+                    category=attr.get("category", "default"),
+                )
+                if (
+                    await db.execute(select(Attribute).where(Attribute.uuid == object_attribute.uuid))
+                ).scalars().first() is None:
+                    db.add(object_attribute)
+        logger.info(f"Processed Event: {new_event}")
+        await db.flush()
+        return new_event
+
+    except json.JSONDecodeError as e:
+        logger.error(f"JSON parsing error: {e}")
+        raise ValueError(f"Invalid JSON format: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected error in processmisp_job: {e}")
+        raise
 
 
 def processcsv_job(user: UserData, string_to_process: str) -> list[Attribute]:
