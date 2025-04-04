9bef43c52db852aa2e0d7076dfef232295e0a78e
Amadeus Haessler <mail@amadeushaessler.de>
2025-04-04 17:35:10 +0200

    fixed push job tests

--- tests/with_remote_misp_only/unittests/sync/push/test_push.py
+++ tests/with_remote_misp_only/unittests/sync/push/test_push.py
@@ -1,13 +1,9 @@
-import time
-from time import sleep
 from uuid import UUID
 
 import pytest
-from sqlalchemy import select
 
-from mmisp.api_schemas.events import AddEditGetEventDetails
-from mmisp.api_schemas.server import Server, ServerVersion
-from mmisp.db.models.event import Event
+from mmisp.api_schemas.events import AddEditGetEventDetails, EditEventBody
+from mmisp.api_schemas.server import Server
 from mmisp.worker.api.requests_schemas import UserData
 from mmisp.worker.jobs.sync.push.job_data import PushData, PushResult, PushTechniqueEnum
 from mmisp.worker.jobs.sync.push.push_job import push_job
@@ -48,22 +44,16 @@ async def test_push_edit_event_full(init_api_config, db, misp_api, user, remote_
 
     server: Server = await get_server(db, remote_misp.id)
 
-    print("bonobo_sync_test_event: ", vars(sync_test_event))
-
-    print("bonobo_remote_server: ", vars(remote_misp))
-
-    server_version: ServerVersion = await misp_api.get_server_version(server)
-    print("bonobo_server_version: ", vars(server_version))
-
-    event_to_update = await misp_api.get_event(UUID(sync_test_event.uuid))
+    event_to_update = await misp_api.get_event(UUID(sync_test_event.uuid), server)
     assert event_to_update
 
-    sleep(5)
     # edit event
     event_to_update.info = "edited_" + event_to_update.info
     # event_to_update.timestamp = str(int(time.time()))
     # event_to_update.publish_timestamp = str(int(time.time()))
     await db.commit()
+
+    await misp_api.update_event(event_to_update)
 
     push_result: PushResult = push_job.delay(user_data, push_data).get()
     assert push_result.success
@@ -80,62 +70,28 @@ async def test_push_edit_event_incremental(
     user_data: UserData = UserData(user_id=user.id)
     push_data: PushData = PushData(server_id=remote_misp.id, technique=PushTechniqueEnum.INCREMENTAL)
 
-    try:
-        statement = select(Event)
-        result: list[Event] = (await remote_db.execute(statement)).scalars()
-
-        for event in result:
-            print("bananenbieger_test_push_edit_event_incremental_remote_events_before_push: ", vars(event))
-
-    except Exception:
-        print("bananenbieger_test_push_edit_event_incremental_remote_events_before_push", "no remote events")
-
-    try:
-        statement = select(Event)
-        result = (await db.execute(statement)).scalars()
-
-        for event in result:
-            print("bananenbieger_test_push_edit_event_incremental_local_events: ", vars(event))
-
-    except Exception:
-        print("bananenbieger_test_push_edit_event_incremental_local_events", "no remote events")
-
     push_result: PushResult = push_job.delay(user_data, push_data).get()
     assert push_result.success
 
     server: Server = await get_server(db, remote_misp.id)
 
-    print("bananenbieger_test_push_edit_event_incremental_event_fixture", vars(sync_test_event))
+    event_to_update = await misp_api.get_event(UUID(sync_test_event.uuid))
+    assert event_to_update
 
-    statement = select(Event.uuid)
-    result = (await remote_db.execute(statement)).scalars()
-    print("bananenbieger_test_push_edit_event_incremental_remote_events_after_push", list(result))
-
-    assert sync_test_event.uuid == (await misp_api.get_event(UUID(sync_test_event.uuid), server)).uuid
-
-    sleep(5)
     # edit event
-    sync_test_event.info = "edited" + sync_test_event.info
-    sync_test_event.timestamp = str(int(time.time()))
-    sync_test_event.publish_timestamp = str(int(time.time()))
+    event_to_update.info = "edited" + sync_test_event.info
+    event_to_update.timestamp = None
+    event_to_update.publish_timestamp = None
+    # event_to_update.timestamp = str(int(time.time()))
+    # event_to_update.publish_timestamp = str(int(time.time()))
+    await db.commit()
+
+    await misp_api.update_event(EditEventBody.parse_obj(event_to_update.dict()))
     await db.commit()
 
     push_result: PushResult = push_job.delay(user_data, push_data).get()
     assert push_result.success
 
     # tests if event was updated on remote-server
-    remote_event: AddEditGetEventDetails = misp_api.get_event(UUID(sync_test_event.uuid))
-    assert remote_event.info == sync_test_event.info
-
-
-@pytest.mark.asyncio
-async def test_push_edit_event_incremental(
-        init_api_config, db, misp_api, remote_misp, remote_db, remote_event, user
-):
-    user_data: UserData = UserData(user_id=user.id)
-    push_data: PushData = PushData(server_id=remote_misp.id, technique=PushTechniqueEnum.FULL)
-
-    push_result: PushResult = push_job.delay(user_data, push_data).get()
-    assert push_result.success
-    server: Server = await get_server(db, remote_misp.id)
-    updated_remote_event: AddEditGetEventDetails = await misp_api.get_event(UUID(remote_event.uuid), server)
+    remote_event: AddEditGetEventDetails = await misp_api.get_event(UUID(event_to_update.uuid), server)
+    assert remote_event.info == event_to_update.info
