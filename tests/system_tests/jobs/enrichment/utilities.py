8f7ff0b6dd3c51d75dc6b4b993e51dfbc9b70565
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-04-04 12:08:59 +0200

    update pre-commit-config

--- tests/system_tests/jobs/enrichment/utilities.py
+++ tests/system_tests/jobs/enrichment/utilities.py
@@ -7,9 +7,9 @@ from mmisp.plugins.enrichment.enrichment_plugin import EnrichmentPluginInfo
 def is_plugin_available(plugin_name: str, client: TestClient, authorization_headers) -> bool:
     get_plugins_url: str = "/worker/enrichment/plugins"
     get_plugins_response: Response = client.get(get_plugins_url, headers=authorization_headers)
-    assert (
-        get_plugins_response.status_code == 200
-    ), f"Enrichment Plugins could not be fetched. {get_plugins_response.json()}"
+    assert get_plugins_response.status_code == 200, (
+        f"Enrichment Plugins could not be fetched. {get_plugins_response.json()}"
+    )
 
     for plugin in get_plugins_response.json():
         if EnrichmentPluginInfo.parse_obj(plugin).NAME == plugin_name:
