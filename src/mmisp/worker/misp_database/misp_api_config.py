4f7359c452efb2bfc42faed90757d10c16662640
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-04-28 23:51:32 +0200

    fix misp_api_config

--- src/mmisp/worker/misp_database/misp_api_config.py
+++ src/mmisp/worker/misp_database/misp_api_config.py
@@ -1,6 +1,5 @@
-from pydantic import Field, NonNegativeFloat, constr
+from pydantic import Field, NonNegativeFloat
 from pydantic_settings import BaseSettings
-from typing_extensions import Annotated
 
 ENV_MISP_API_URL: str = "DB_API_URL"
 ENV_MISP_API_KEY: str = "DB_API_KEY"
@@ -9,7 +8,7 @@ ENV_MISP_API_READ_TIMEOUT: str = "MISP_API_READ_TIMEOUT"
 
 
 class MispAPIConfigData(BaseSettings):
-    url: Annotated[str, constr(regex="^https?://\\w")] = Field("http://127.0.0.1", env=ENV_MISP_API_URL)
+    url: str = Field("http://127.0.0.1", pattern="^https?://\\w", env=ENV_MISP_API_URL)
     key: str = Field("", env=ENV_MISP_API_KEY)
     connect_timeout: NonNegativeFloat = Field(40, env=ENV_MISP_API_CONNECT_TIMEOUT)
     read_timeout: NonNegativeFloat = Field(40, env=ENV_MISP_API_READ_TIMEOUT)
