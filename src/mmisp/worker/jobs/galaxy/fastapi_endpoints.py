25ac4574826f14ac449a58d8b4aa4eca6ed6f153
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-05-14 08:44:40 +0200

    add all galaxy fastapi_endpoints

--- src/mmisp/worker/jobs/galaxy/fastapi_endpoints.py
+++ src/mmisp/worker/jobs/galaxy/fastapi_endpoints.py
@@ -3,17 +3,17 @@ from typing import Optional
 from fastapi import Depends
 
 from mmisp.worker.api.api_verification import verified
-from mmisp.worker.api.job_router import job_router
+from mmisp.worker.api.job_router import job_router, translate_job_result
 from mmisp.worker.api.requests_schemas import UserData
 from mmisp.worker.api.response_schemas import CreateJobResponse
 from mmisp.worker.controller import job_controller
 from mmisp.worker.jobs.galaxy.import_galaxies_job import import_galaxies_job
-from mmisp.worker.jobs.galaxy.job_data import CreateGalaxiesImportData
+from mmisp.worker.jobs.galaxy.job_data import CreateGalaxiesImportData, ImportGalaxiesResult
 
 from .queue import queue
 
 
-@job_router.post("/importGalaxies", dependencies=[Depends(verified)])
+@job_router.post("/importGalaxies", dependencies=[Depends(verified)], tags=["importGalaxies"])
 async def create_galaxies_import_job(
     user: UserData, data: Optional[CreateGalaxiesImportData] = None
 ) -> CreateJobResponse:
@@ -29,3 +29,18 @@ async def create_galaxies_import_job(
     data = data or CreateGalaxiesImportData()
     async with queue:
         return await job_controller.create_job(queue, import_galaxies_job, user, data)
+
+
+@job_router.get("/importGalaxies/{task_id}", dependencies=[Depends(verified)], tags=["importGalaxies"])
+async def get_free_text_job_result(task_id: str) -> ImportGalaxiesResult:
+    return await translate_job_result(queue, task_id)
+
+
+@job_router.get("/importGalaxies/{task_id}/status", dependencies=[Depends(verified)], tags=["importGalaxies"])
+async def get_free_text_job_status(task_id: str) -> ImportGalaxiesResult:
+    return await job_controller.get_job_status(queue, task_id)
+
+
+@job_router.delete("/importGalaxies/{task_id}", dependencies=[Depends(verified)], tags=["importGalaxies"])
+async def abort_free_text_job(task_id: str) -> bool:
+    return await job_controller.cancel_job(queue, task_id)
