1fe96612835af63eb3ea064f0a1ccab863bf3e4f
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-05-23 19:45:33 +0200

    sync config data to pydantic settings

--- src/mmisp/worker/jobs/sync/sync_config_data.py
+++ src/mmisp/worker/jobs/sync/sync_config_data.py
@@ -1,44 +1,19 @@
-import os
-from typing import Self
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
-from mmisp.worker.config import ENV_PREFIX, ConfigData
-
-ENV_ENABLE_EVENT_BLOCKLISTING = f"{ENV_PREFIX}MISP.enableEventBlocklisting"
-ENV_ENABLE_ORG_BLOCKLISTING = f"{ENV_PREFIX}MISP.enableOrgBlocklisting"
-ENV_HOST_ORG_ID = f"{ENV_PREFIX}MISP.host_org_id"
-
-DEFAULT_ENABLE_EVENT_BLOCKLISTING = False
-DEFAULT_ENABLE_ORG_BLOCKLISTING = False
-DEFAULT_HOST_ORG_ID = 0
+ENV_ENABLE_EVENT_BLOCKLISTING = "MISP.enableEventBlocklisting"
+ENV_ENABLE_ORG_BLOCKLISTING = "MISP.enableOrgBlocklisting"
+ENV_HOST_ORG_ID = "MISP.host_org_id"
 
 
-class SyncConfigData(ConfigData):
+class SyncConfigData(BaseSettings):
     """
     Encapsulates the configuration of the sync workers.
     """
 
-    misp_enable_event_blocklisting: bool = DEFAULT_ENABLE_EVENT_BLOCKLISTING
-    misp_enable_org_blocklisting: bool = DEFAULT_ENABLE_ORG_BLOCKLISTING
-    misp_host_org_id: int = DEFAULT_HOST_ORG_ID
-
-    def read_config_from_env(self: Self) -> None:
-        """
-        Reads the configuration of the sync workers from environment variables.
-        :return: None
-        """
-
-        event_blocklist: str = os.getenv(ENV_ENABLE_EVENT_BLOCKLISTING, "")
-        if event_blocklist:
-            self.misp_enable_event_blocklisting = event_blocklist.lower() == "true"
-
-        org_blocklist: str = os.getenv(ENV_ENABLE_ORG_BLOCKLISTING, "")
-        if org_blocklist:
-            self.misp_enable_org_blocklisting = org_blocklist.lower() == "true"
-
-        host_org_id: str = os.getenv(ENV_HOST_ORG_ID, "")
-        if host_org_id:
-            self.misp_host_org_id = int(host_org_id)
+    misp_enable_event_blocklisting: bool = Field(False, validation_alias=ENV_ENABLE_EVENT_BLOCKLISTING)
+    misp_enable_org_blocklisting: bool = Field(False, validation_alias=ENV_ENABLE_ORG_BLOCKLISTING)
+    misp_host_org_id: int = Field(0, validation_alias=ENV_HOST_ORG_ID)
 
 
 sync_config_data: SyncConfigData = SyncConfigData()
-sync_config_data.read_config_from_env()
