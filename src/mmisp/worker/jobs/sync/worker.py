540577ef96c6e27f9f45ef7b8bf0a78c4f3668ab
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-05-28 15:34:26 +0200

    add log middleware

--- src/mmisp/worker/jobs/sync/worker.py
+++ src/mmisp/worker/jobs/sync/worker.py
@@ -1,7 +1,10 @@
 import mmisp.db.all_models  # noqa: F401
+from mmisp.worker.node.middlewares import log_middleware
 
 from .pull import pull_job
 from .push import push_job
 from .queue import queue
+
+queue.middleware(log_middleware)
 
 __all__ = ["queue", "pull_job", "push_job"]
